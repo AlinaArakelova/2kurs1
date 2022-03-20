76361553d0dcb16ab08a31349e5e50906f510cde
AlinaArakelova <danikalina@gmail.com>
2022-03-20 17:42:39 +0300

    fix formatting

---
+++ src/com/company/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MyArrayDataException extends Exception {
+    int i;
+    int j;
+
+    public MyArrayDataException (int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+}
